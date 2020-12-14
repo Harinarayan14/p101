2b15901280f061f204a763391ba37105f516cc86
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2020-12-14 17:18:28 +0530

    Create uploadFiles.py

---
+++ uploadFiles.py
@@ -0,0 +1,32 @@
+import os
+import dropbox
+from dropbox.files import WriteMode
+
+class Upload:
+    def __init__(self,access_token):
+        self.access_token =  access_token
+
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+        for root, files in os.walk(file_from):
+            for filename in files:  
+
+                local_path = os.path.join(root, filename)
+
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+
+def main():
+    access_token = '*****' # Use access token
+    upload = Upload(access_token)
+
+    file_from = str(input("Enter the folder path to transfer : "))
+    file_to = str(input("Enter the full path to upload to dropbox: "))  
+    
+    upload.upload_file(file_from,file_to)
+    print("File has been moved !!!")
+
+main()
